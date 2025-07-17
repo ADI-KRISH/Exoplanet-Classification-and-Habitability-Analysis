937d55a0a520e8022861e7a1dc701f0b43f352cd
GS ADITHYA KRISHNA <adikrishactive@gmail.com>
2025-07-17 18:06:42 +0530

    Update PlanetReportPage.js

--- ml/src/PlanetReportPage.js
+++ ml/src/PlanetReportPage.js
@@ -53,7 +53,7 @@ ${habitabilityFeatures.map((val, i) => `${habitFeatureDescriptions[i]}: ${val}`)
 
 
         try {
-          const res = await fetch('http://localhost:8000/gemini-explanation', {
+            const response = await fetch('https://exoplanet-classification-and-hol4.onrender.com/predict', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message })
